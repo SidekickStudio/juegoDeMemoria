ce5f5cc74bc19006fb91316bf5dc4bdcff1db22f
Jose Luis Castro <josecastro@sidekick.pe>
2016-09-15 14:08:24 -0500

    Init Project: Juego de Memoria

---
+++ JuegoDeMemoria.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+ By: José Luis Castro
+ Site: Sidekick.pe
+*/
+
+//================================
+// --- Juego de Memoria
+//================================
+
+var n = 0
